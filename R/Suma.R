e39dd9c59fee39428c7e7fb861f8ee3c8c1caf81
bautitorre12 <bautistatorregiani@gmail.com>
2024-10-18 11:02:19 -0300

    agrego todo

--- R/Suma.R
+++ R/Suma.R
@@ -1,22 +1,31 @@
 # Suma de dos números
+#'
+#'' La función `suma()` dos valores numéricos positivos y calcula la suma entre ambos números.
+#'
+#' @param x vector numerico
+#' @param y vector numerico
+#'
+#' @return un numero entero
+#' @export
+#'
+#' @examples
+#' suma(2,2)
+#'
 suma <- function(x = 5, y = 2) {
 
-  if (!is.numeric(x)) {
-    stop("El argumento 'x' debe ser numérico")
+    if (!is.numeric(x) || !is.numeric(y)) {
+      stop("Los argumentos deben ser numéricos")
+    }
+
+    if (sign(x) == -1) {
+      stop("No puedo sumar negativos: el argumento 'x' es negativo")
+    }
+    if (sign(y) == -1) {
+      stop("No puedo sumar negativos: el argumento 'y' es negativo")
+    }
+
+    return(x + y)
   }
 
-  if (!is.numeric(y)) {
-    stop("El argumento 'y' debe ser numérico")
-  }
 
-  if (sign(x) == -1) {
-    stop("No puedo sumar negativos: el argumento 'x' es negativo")
-  }
-  if (sign(y) == -1) {
-    stop("No puedo sumar negativos: el argumento 'y' es negativo")
-  }
-
-
-  return(x + y)
-}
 
---
+++ README.md
@@ -0,0 +1,29 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# paquetedeprueba
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of paquetedeprueba is to …
+
+## Installation
+
+You can install the development version of paquetedeprueba from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("BautistaTorregiani/paquetedeprueba")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(paquetedeprueba)
+suma(2,2)
+#> [1] 4
+```
